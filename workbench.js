546554e752c29b00e637ca6b6332871338726cd7
Kobi Gurkan <kobigurk@gmail.com>
2016-08-03 10:17:16 +0300

    Adds default and configurable initial state

--- workbench.js
+++ workbench.js
@@ -1,13 +1,57 @@
 var path = require('path');
+var fs = require('fs');
 
 var Pudding = require('ether-pudding');
 var Sandbox = require('ethereum-sandbox-client');
 var helper = require('ethereum-sandbox-helper');
 
-var Workbench = function(defaults) {
+function configureState(options, ethereumJsonPath) {
+  var state;
+  if (options.initialState) {
+    console.log('Using initialState from options');
+    state = {
+      contracts: 'contracts',
+      env: options.initialState
+    };
+    fs.writeFileSync(ethereumJsonPath, JSON.stringify(state));
+  }
+
+  if (!fs.existsSync(ethereumJsonPath)) {
+    console.log('Ethereum json doesn\'t exist, generating');
+    var defaultAccount = '0xcd2a3d9f938e13cd947ec05abc7fe734df8dd826';
+    if (options.defaults && options.defaults.from) defaultAccount = options.defaults.from;
+    state = {
+      contracts: 'contracts',
+      env: {
+        block: {
+          coinbase: defaultAccount,
+          difficulty: '0x0100',
+          gasLimit: 314159200,
+          gasPrice: 600000000
+        },
+        accounts: {}
+      }
+    };
+    state.env.accounts[defaultAccount] = {
+      name: 'fellow-1',
+      balance: 1000000000000000000000000,
+      nonce: '1430',
+      pkey: 'cow',
+      default: true
+    };
+    fs.writeFileSync(ethereumJsonPath, JSON.stringify(state));
+  }
+}
+
+var Workbench = function(options) {
   this.sandbox = new Sandbox('http://localhost:8554');
   this.readyContracts = {};
-  this.defaults = defaults;
+  if (!options) options = {};
+  this.defaults = options.defaults;
+  this.contractsDirectory = options.contratcsDirectory;
+  this.ethereumJsonPath = path.dirname(module.parent.filename) + '/ethereum.json';
+  if (options.ethereumJsonPath) this.ethereumJsonPath = options.ethereumJsonPath;
+  configureState(options, this.ethereumJsonPath);
 };
 
 Workbench.prototype.compile = function(contracts, dir, cb) {
@@ -27,7 +71,7 @@ Workbench.prototype.compile = function(contracts, dir, cb) {
 
 Workbench.prototype.start = function(contracts, cb) {
   var self = this;
-  this.sandbox.start(path.dirname(module.parent.filename) + '/ethereum.json', function (err) {
+  this.sandbox.start(this.ethereumJsonPath, function (err) {
     if (err) return cb(err);
     Object.keys(contracts).forEach(contractName => {
       contracts[contractName].setProvider(self.sandbox.web3.currentProvider);
@@ -41,14 +85,11 @@ Workbench.prototype.stop = function(cb) {
   this.sandbox.stop(cb);
 };
 
-Workbench.prototype.startTesting = function(contracts, dir, cb) {
+Workbench.prototype.startTesting = function(contracts, cb) {
   var self = this;
-  if (typeof dir === 'function') {
-    cb = dir;
-    dir = null;
-  }
   if (typeof contracts === 'string') contracts = [contracts];
   contracts = contracts.map(x => x + '.sol');
+  var dir = this.contractsDirectory;
   if (!dir) dir = './contract';
 
   this.readyContracts = this.compile(contracts, dir);
@@ -62,7 +103,7 @@ Workbench.prototype.startTesting = function(contracts, dir, cb) {
     after(function(done) {
       self.stop(done);
     });
-    cb(self.readyContracts);
+    if (cb) cb(self.readyContracts);
   });
 };
 
